59e790eaceef346b66caec80ada0b10dbc60e87c
Rodney Bautista <Rodney.Bautista92@outlook.com>
2022-02-08 07:48:48 -0800

    for loop basic assignemnt

---
+++ for_loop.py
@@ -0,0 +1,36 @@
+
+for x in range(0,151,1):
+    print(x)
+
+#Multiples of Five
+for y in range(5,1005,5):
+    print(y)
+
+#Counting
+for i in range(1,101,1):
+    if i / 5:
+        print("Coding")
+    if i / 10:
+        print("Coding Dojo")
+
+
+#Whoa. That's Sucker's Huge
+for z in range(1,500001,2):
+    if z % 2 != 0:
+        print(z + z)
+
+#Countdown by Fours
+for i in range(2018,0,-4):
+    if i % 2 == 0:
+        print(i)
+
+#Flexible Counter
+lowNum = 2
+highNum = 9
+mult = 3
+for i in range(lowNum,highNum,mult):
+    print(i + 1)
+
+
+
+
